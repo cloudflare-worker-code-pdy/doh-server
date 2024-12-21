2a5731d43a90139753dab021d2d7c8e86d3a280c
Dickgay <47469884+Dickgay@users.noreply.github.com>
2024-12-21 16:01:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,25 @@
 export default {
   async fetch(request, env, ctx) {
-    return fetch(
-      request.method === 'GET'
-        ? `https://cloudflare-dns.com/dns-query?${new URL(request.url).searchParams}`
-        : 'https://cloudflare-dns.com/dns-query',
-      {
+    // 构造多个 DoH 服务的请求 URLs
+    const dohUrls = [
+      `https://cloudflare-dns.com/dns-query?${new URL(request.url).searchParams}`,
+      `https://1.1.1.1/dns-query?${new URL(request.url).searchParams}`,
+      `https://1.0.0.1/dns-query?${new URL(request.url).searchParams}`
+    ];
+
+    // 准备所有请求的 promise
+    const promises = dohUrls.map(url => {
+      return fetch(url, {
         method: request.method,
         body: request.method === 'POST' ? request.body : undefined,
         headers: { 'accept': 'application/dns-message' }
-      }
-    )
+      });
+    });
+
+    // 使用 Promise.race() 来获取最先响应的请求
+    const firstResponse = await Promise.race(promises);
+
+    // 返回最先响应的结果
+    return firstResponse;
   }
 }
